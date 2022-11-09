c397cc44760488ea6578bb1594b138f0f4de8128
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 03:09:59 -0500

    3

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,48 @@
 import java.util.Scanner;
-
+import java.lang.String;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
 
         Scanner sc = new Scanner(System.in);
-        int a;
+        double a;
         System.out.println("Enter a: ");
         a = sc.nextInt();
-        int b;
+        double b;
         System.out.println("Enter b :");
         b = sc.nextInt();
-        int c;
+        double c;
         System.out.println("Enter c :");
         c = sc.nextInt();
+        float x;
+        System.out.println("Enter x (float) : ");
+        x = sc.nextFloat();
 
-        int z = (a+b);
+        float x1 = x;
+        double z1 = (a+b) / (2);
+        System.out.println("z1"+z1);
+        double z2 = (x * x * x);
+        System.out.println("z2"+z2);
+        double z3 = (x * x);
+        System.out.println("z3"+z3);
 
-        System.out.println("" + z);
+        double z4 = (a+b) * (a+b);
+        System.out.println("z4"+z4);
+
+        double z5 = (z1*z2) + z4*z3 +a+b+c;
+        System.out.println("z5"+z5);
+
+        System.out.println("1st: " + (a+b/2));
+        System.out.println("2nd: " + Math.pow(x,3));
+        System.out.println("3rd: " + Math.pow(a+b,2));
+        System.out.println("4th: " + Math.pow(x,2));
+
+       // double ans = (((a+b)/2)*Math.pow(x,3)) + (Math.pow(a+b,2) * Math.pow(x,2)) + a + b + c;
+        //System.out.println("ans: " +ans);
+
+
+        System.out.println("output : " + z5);
 
 
     }
