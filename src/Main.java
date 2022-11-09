0235adbeb049f4caf9decda58b560ac794a81b11
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 01:56:47 -0500

    1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("Enter a: ");
+        a = sc.nextInt();
+        int b;
+        System.out.println("Enter b :");
+        b = sc.nextInt();
+
+
+    }
+}
