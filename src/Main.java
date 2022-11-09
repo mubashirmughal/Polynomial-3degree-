1aa4c44481f804418e7cb2d0ccb1ffd774481b92
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 14:57:12 -0500

    asfa

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.lang.String;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
 
         Scanner sc = new Scanner(System.in);
